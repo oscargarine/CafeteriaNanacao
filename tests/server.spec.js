011bf990fe13ab2d4fa6615df656fdd0ac463548
Oscar Garin <oscargarin@gmail.com>
2025-07-11 12:21:25 -0400

    actualizacion 1.1

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -3,7 +3,7 @@ import app from '../index.js'
 
 
 describe('GET /cafes', () => {
-  it('debería responder con un status 200', async () => {
+  it('Debería responder con un status 200 ("OK")', async () => {
     const response = await request(app).get('/cafes')
     expect(response.status).toBe(200)
   })
@@ -17,53 +17,49 @@ describe('GET /cafes', () => {
   })
 })
 
-describe ('DELETE /cafes', () => {
-  describe('debería responder con status 404 si el ID existe', () => {
-    it('debería repsonder con status 404', async () => {
-      const idErroneo = '9' // un ID que no exista
+describe('DELETE /cafes/:id', () => {
+  it('Debería responder con status 404 ("Página no encontrada") si el ID no existe', async () => {
+    const idErroneo = '9' // un ID que no exista
 
-      // no requerimos de un token jwt
-      const response = await request(app)
-        .delete(`/cafes/${idErroneo}`)
-        .set('Authorization', 'Bearer cualquierToken')
+    // no requerimos de un token jwt
+    const response = await request(app)
+      .delete(`/cafes/${idErroneo}`)
+      .set('Authorization', 'Bearer cualquierToken')
 
-      expect(response.status).toBe(404)
-      expect(response.body.message)
+    expect(response.status).toBe(404)
+    expect(response.body.message)
       .toBe('No se encontró ningún cafe con ese id') // deben ser iguales!
-    })
   })
 })
+
 
 describe('POST /cafes', () => {
-  describe('debería tener la propiedad id', () => {
-    const newCafe = {
-      id: '5', // debemos poner un codigo que no exista en la bd ("cafes.json")
-      nombre: 'Latte'
-    }
+  it('Debería responder con un status 201 ("Creado") al crear un nuevo café', async () => {
 
-    it('debería responder con un status 201', async () => {
-      const response = await request(app).post('/cafes').send(newCafe)
-      expect(response.status).toBe(201)
-    })
+  const newCafe = {
+    id: '5', // debemos poner un codigo que no exista en la bd ("cafes.json")
+    nombre: 'Latte'
+  }
+
+    const response = await request(app).post('/cafes').send(newCafe)
+    expect(response.status).toBe(201)
   })
 })
 
-describe ('PUT /cafes', () => {
-  describe('debería responder con status 400 si el ID no es el mismo', () => {
-    it('debería repsonder con status 400', async () => {
-      const idBueno = '3' // el ID correcto del café
-      const payloadDiferenteId = {
-        id: '6',
-        nombre: 'Mocaccino'
-      }
+describe ('PUT /cafes/:id', () => {
+  it('Debería responder con status 400 ("Solicitud incorrecta") si el ID no es el mismo', async () => {
+    const idBueno = '3' // el ID correcto del café
+    const payloadDiferenteId = {
+      id: '6',
+      nombre: 'Mocaccino'
+    }
 
-      const response = await request(app)
-        .put(`/cafes/${idBueno}`)
-        .send(payloadDiferenteId)
+    const response = await request(app)
+      .put(`/cafes/${idBueno}`)
+      .send(payloadDiferenteId)
 
-      expect(response.status).toBe(400)
-      expect(response.body.message)
+    expect(response.status).toBe(400)
+    expect(response.body.message)
       .toBe('El id del parámetro no coincide con el id del café recibido') // deben ser iguales!
-    })
   })
 })
